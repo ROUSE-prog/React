cbc52fce109559991f6fdae192dbb790c5c9b1b9
Steven Rouse <52251052+ROUSE-prog@users.noreply.github.com>
2023-08-04 08:49:56 -0400

    Delete reactmax directory

--- reactmax/src/components/Homepage.js
+++
@@ -1,6 +0,0 @@
-function HomePage() {
-    return <h1>Welcome to React.js Tutorial App</h1>;
-  }
-  
-  export default HomePage;
-  
--- reactmax/src/components/Topicspage.js
+++
@@ -1,7 +0,0 @@
-function TopicsPage() {
-    // Here you could map through your topics and create links or buttons to each topic's page
-    return <h1>Topics</h1>;
-  }
-  
-  export default TopicsPage;
-  
